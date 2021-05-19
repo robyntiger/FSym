3d44b67602f9fea5fb397072ba480c1709cf6d1e
Robyn L <robyn.leinster@btinternet.com>
2021-05-20 00:18:46 +0100

    allow user to split string by emoticons in lexer.py

--- lexer.py
+++ lexer.py
@@ -1,7 +1,35 @@
 # take raw source code as series of characters
 # group it into a series of chunks called tokens
+import re
 
 class Lexer:
-    def run_prompt():
-        # take input (file of code)
-        print("bruh")
+    # prints each char
+    def scanner(self, code):
+        i = 1
+        for char in code:
+            print('char ' + str(i) + ":" + char)
+            i+1
+
+    # initial lexer, splits only by whitespace
+    def lexer(self, code):
+        lexeme = ''
+        for i in range(len(code)):
+            if code[i] == ' ':
+                print(lexeme)
+                lexeme = ''
+            else:
+                lexeme = lexeme + code[i]
+        print(lexeme)
+
+    # experimental lexer thingy
+    def lextest(self, code):
+        # keywords regex
+        KEYWORDS = '(xP|:D|:O|:\)|\(:|;\)|;D|;O)'
+        test =re.split(KEYWORDS, code)
+        if test[-1] == '':
+            test.pop()
+
+        print(test)
+
+test = Lexer()
+test.lextest('11xP hi :D')
