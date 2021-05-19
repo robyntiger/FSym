fa2f14264a8bb2551f6f0d1a85b37be88ed93e51
Robyn L <robyn.leinster@btinternet.com>
2021-05-20 00:17:53 +0100

    Allow user to split strings by list of emoticons

---
+++ lexer.py
@@ -0,0 +1,7 @@
+# take raw source code as series of characters
+# group it into a series of chunks called tokens
+
+class Lexer:
+    def run_prompt():
+        # take input (file of code)
+        print("bruh")
