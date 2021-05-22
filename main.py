9c4bd364c1c31c0ef9a4f991f561eae1a0b9483e
Robyn L <robyn.leinster@btinternet.com>
2021-05-22 23:46:35 +0100

    added more commands & fixed readme.md

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import re
 
 class Run:
     def __init__(self):
-        self.COMMAND = '(:\)|:S|:@|xp|:D|:3|^^|:\(|:p|:&|:O|xD|;\)|\(;|8\)|\(8|B\)|:L|:d|:v|:\]|\[:|:\'\(|:#| )'
-        self.stack = []
+        self.COMMAND = '(:\)|;\)|:&|:S|:3|:D|:/|:#| )'
+        self.stack = [97]
         self.prog = []
 
     # splits a program up by command
@@ -21,9 +21,8 @@ class Run:
         self.prog = [item for item in self.prog if item.strip()]
 
     # pushes to stack
-    def stack_push(self, *arg):
-        for item in arg:
-            self.stack.append(item)
+    def stack_push(self, item):
+        self.stack.append(item)
 
     # pops stack and returns item
     def stack_pop(self):
@@ -37,27 +36,28 @@ class Run:
 
     def run_commands(self, txt):
         self.split(txt)
+        print(self.prog)
 
+        # new switcher yay
         switcher = {
             ':)': lambda: self.stack_pop(),
-            ':@': lambda: self.stack_push(int(self.stack_pop()) + int(self.stack_pop())),
-            ':$': lambda: self.stack_push(int(self.stack_pop()) - int(self.stack_pop())),
-            'xp': lambda: self.stack_push(int(self.stack_pop()) * int(self.stack_pop())),
-            ':D': lambda: self.stack_push(int(self.stack_pop()) / int(self.stack_pop())),
-            ':3': lambda: self.stack_push(int(self.stack_pop()) % int(self.stack_pop())),
-            '^^': lambda: self.stack_push(int(self.stack_pop())**2),
-            ':(': lambda: self.stack_push(int(self.stack_pop())**(1/2)),
-            #':p': lambda: self.stack_push(not int(self.stack_pop())),
-            #':&': lambda: self.stack_push(bool((self.stack_pop() and self.stack_pop()))),
-            #':O': lambda: self.stack_push(int(self.stack_pop()) or int(self.stack_pop())),
-            ';)': lambda: [print(item) for item in self.stack]
+            ';)': lambda: self.stack_push(self.stack[-1]),
+            ':&': lambda: self.stack_push(self.stack_pop()+self.stack_pop()),
+            ':S': lambda: self.stack_push(self.stack_pop()-self.stack_pop()),
+            ':3': lambda: self.stack_push(self.stack_pop()*self.stack_pop()),
+            ':/': lambda: self.stack_push(self.stack_pop()/self.stack_pop()),
+            ':D': lambda: self.stack_push(int(1)),
+            ':o': lambda: print(self.stack_pop()),
+            ':@': lambda: print(chr(self.stack_pop())),
+            ':#': lambda: [print(item) for item in self.stack]
         }
 
         for item in self.prog:
-            try:
+            switcher[item]()
+            '''try:
                 switcher[item]()
             except:
-                self.stack_push(item) # pushes num to stack
+                self.stack_push(item) # pushes num to stack'''
 
 test = Run()
-test.run_commands("0 61 :& ;)")
+test.run_commands(":@")
--- readme.md
+++ readme.md
@@ -11,13 +11,13 @@ Command | Description
 :& | Pop top two items in stack, adds them, then pushes result to stack
 :S | Pop top two items in stack,  subtracts the first by the second, then pushes result to stack
 :3 | Pop top two items in stack, multiplies them, then pushes result to stack
-:D | Pop top two items in stack, divides the first by the second, then pushes result to stack
+:/ | Pop top two items in stack, divides the first by the second, then pushes result to stack
 xD | Pop top item in stack, not's it, then pushes result to stack
 x3 | Pop top two items in stack, and's them, then pushes result to stack
 xP | Pop top two items in stack, or's them, then pushes result to stack
 :p | Loops while top item in stack is true (not 0)
 :d | Ends while loop
-:/ | Take user input and push to stack
+:D | Push 1 to stack
 :o | Pop top item in stack and print as int
 :@ | Pop top item in stack and print as ASCII character
 :( | End the program
