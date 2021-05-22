c19e64bdae151ad594091b8c5e69557627c68a95
Robyn L <robyn.leinster@btinternet.com>
2021-05-22 02:06:33 +0100

    trying to make the commands work

--- functions.py
+++ functions.py
@@ -1,1 +1,35 @@
 # functions
+
+stack = [1, 2, 3]
+
+# pushes to stack
+def stack_push(self, *arg):
+    for item in arg:
+        self.stack.append(item)
+
+# pops stack and returns item
+def stack_pop(self):
+    item = self.stack[-1]
+    self.stack.pop()
+
+    return item
+
+# adds top two numbers in stack
+def add(self):
+    self.stack_push(int(self.stack_pop()) + int(self.stack_pop()))
+
+def run_commands(self):
+    switcher = {
+        ':)': self.stack_pop,
+        ':@': self.add,
+    }
+
+    test = [":)"]
+
+    for item in test:
+        try:
+            switcher[item]()
+        except:
+            self.stack_push(item)
+
+print(stack)
--- main.py
+++ main.py
@@ -10,55 +10,51 @@ import re
 class Run:
     def __init__(self):
         self.COMMAND = '(:\)|:S|:@|xp|:D|:3|^^|:\(|:p|:&|:O|xD|;\)|\(;|8\)|\(8|B\)|:L|:d|:v|:\]|\[:|:\'\(|:#| )'
-        self.stack = [1, 2, 3]
+        self.stack = []
         self.prog = []
 
     # splits a program up by command
     def split(self, program):
         self.prog = re.split(self.COMMAND, program)
 
-        # for some reason it adds empty spaces and i dunno why
-        # so get rid of them
-        for item in self.prog:
-            if item == '':
-                self.prog.remove('')
+        # remove empty lines & spaces
+        self.prog = [item for item in self.prog if item.strip()]
 
     # pushes to stack
-    def push(self, *arg):
+    def stack_push(self, *arg):
         for item in arg:
             self.stack.append(item)
 
     # pops stack and returns item
-    def pop(self):
+    def stack_pop(self):
         item = self.stack[-1]
         self.stack.pop()
 
         return item
 
-    # for debugging hhh
-    def debug(self):
-        print("BRUH")
+    # adds top two numbers in stack
+    def add(self):
+        self.stack_push(int(self.stack_pop()) + int(self.stack_pop()))
 
-    def run_commands(self, program):
+    def run_commands(self, txt):
+        self.split(txt)
+
         switcher = {
-            ':)': self.pop,
-            ':S': self.debug,
-            ':@': lambda a, b: self.push(a+b),
-            ':$': self.debug,
-            'xp': self.debug
+            ':)': self.stack_pop,
+            ':@': self.add,
         }
 
-        self.split(program)
+        print(self.prog)
 
         for item in self.prog:
             try:
-                switcher[item](1, 4)
-            except KeyError:
-                # push
-                pass
+                # runs command
+                switcher[item]()
+            except:
+                # if it's not a command, it's assumed to be a number
+                self.stack_push(item)
 
         print(self.stack)
 
-
 test = Run()
-test.run_commands(":)")
+test.run_commands("2 2        :@")
